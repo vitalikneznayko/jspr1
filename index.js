11b6135252bfb835bebea5e50430b20675ad4e7c
vitalikneznayko <neznajko@class.ztk.org.ua>
2023-02-27 10:09:20 +0200

    1

---
+++ index.js
@@ -0,0 +1,6 @@
+const Name = prompt("Введіть ваше ім'я:");
+const LastName = prompt("Введіть ваше призвіще:");
+const Age = prompt("Введіть ваш вік");
+
+
+console.log(`Мене звати ${Name} ${LastName} та мені ${Age} років !!!`);
